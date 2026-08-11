e52234d420112b19f5f949288a24cc5a0652c64f
lio311 <lior31197@gmail.com>
2026-08-11 16:57:03 +0300

    fix: next.config.ts remote patterns for images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'libero-il.co.il',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.libero-il.co.il',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
