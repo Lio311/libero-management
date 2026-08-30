b78f8c4f2764c342809e0d91227003de951d770e
lio311 <lior31197@gmail.com>
2026-08-30 13:56:23 +0300

    fix: remove scratch script breaking build

--- scratch/fetch_wc_meta.ts
+++
@@ -1,6 +0,0 @@
-import { fetchFromWooCommerce } from "../src/lib/wc-config";
-async function run() {
-  const orders = await fetchFromWooCommerce("orders", "per_page=1", "velour");
-  console.log(JSON.stringify(orders[0].meta_data, null, 2));
-}
-run();
