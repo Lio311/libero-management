b5c4d7ab1d4c0c9304cd37e54b97805c6ef5a4bd
lio311 <lior31197@gmail.com>
2026-09-24 11:52:10 +0300

    chore: remove temporary debug files

--- query.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'; // Add this line
-import { getCustomerHistory } from './src/lib/customer-history';
-
-async function run() {
-  const history = await getCustomerHistory('iv64674@gmail.com', '0542378236', 0);
-  console.log("History orders:", history.pastOrders.map(o => ({ id: o.id, date: o.dateCreated, status: o.status })));
-  process.exit(0);
-}
-run();
